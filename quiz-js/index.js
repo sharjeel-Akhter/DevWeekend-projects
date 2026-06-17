15b61c2a66470ba2287b8778bc31a363f6f0806f
Sharjeel <bsf24002924@ue.edu.pk>
2026-06-17 12:37:35 +0500

    done

---
+++ quiz-js/index.js
@@ -0,0 +1,32 @@
+let answers = ["A","A","A","A"];
+
+let form = document.querySelector(".quiz-form");
+let result = document.querySelector(".result");
+let question = document.querySelectorAll(".question")
+let score = 0;
+let isSubmit = false;
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const userAns = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+        if (isSubmit) return;
+        
+        isSubmit = true;
+        userAns.forEach((ans, index) => {
+            if (ans === answers[index]) {
+                score++;
+                question[index].classList.add("correct")
+            } else {
+                question[index].classList.add("wrong")
+            }
+        })
+        resDisplay();
+       
+    })
+
+
+function resDisplay(){
+    result.classList.remove("hide")
+    result.querySelector(".detail").innerText = `You Score is ${score}/5`;
+}
