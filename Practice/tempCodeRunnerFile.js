c9a8ac8a4b3365ff3d93fe04e216bcd4ffc6f5a9
Sharjeel <bsf24002924@ue.edu.pk>
2026-07-05 09:38:47 +0500

    deleted tempfile

--- Practice/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-let s = "A man, a plan, a canal: Panama"
-
-// let a = s.toLowerCase().replace(/[^a-z]/g, "")
-//     let arr = a.split("");
-// let lastIndex = arr[(arr.length - 1)];
-// console.log(lastIndex)
-// console.log(a)
-// console.log(typeof a)
