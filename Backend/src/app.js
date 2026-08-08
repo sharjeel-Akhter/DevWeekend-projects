2d9851b9dac60e61bcb62f13458dc46600b0c0df
Sharjeel <bsf24002924@ue.edu.pk>
2026-08-08 14:24:05 +0500

    learning

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -18,7 +18,37 @@ app.post("/notes", async (req, res)=>{
 })
 
 app.get("/notes", async(req, res)=>{
-    
+    const notes = await noteModel.find()
+    res.status(200).json({
+        message:"notes fetched Successfully",
+        notes:notes,
+    })
+})
+
+app.delete("/notes/:id", async (req, res) => {
+    const id = req.params.id
+
+    await noteModel.deleteOne({
+        _id: id,
+    })
+
+    res.status(200).json({
+        message:"Note Deleted Successfully"
+    })
+})
+
+app.patch("/notes/:id", async (req, res) => {
+    const id = req.params.id
+    const description = req.body.description
+    await noteModel.findOneAndUpdate({
+        _id:id
+    }, {
+        description:description
+    })
+
+    res.status(200).json({
+        message:"Note Updated Successfully"
+    })
 })
 
 module.exports = app
